47608e4e7e677e30c4b1d4b25fd76252f9c90478
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-05-08 19:20:50 -0500

    started working on API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  console.log(response.data);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function searchCity(city) {
+  let apiKey = "7689111d9btfeef8e4e3cc0cba8be0o7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector('#search-form-input');
+  let cityElement = document.querySelector('#city');
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector('#search-form');
+searchFormElement.addEventListener('submit', handleSearchSubmit);
