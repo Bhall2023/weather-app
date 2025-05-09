aafc04e902a2b97b0a63b889dc827ede94e7aa42
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-05-08 20:27:33 -0500

    changed default search city

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ function refreshWeather(response) {
 function searchCity(city) {
   let apiKey = "7689111d9btfeef8e4e3cc0cba8be0o7";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
   axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector('#search-form-input');
-  
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
+
+searchCity ("Miami");
